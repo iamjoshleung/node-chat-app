9abda2360eca0214aa10aed46fc59992cda1679e
joshuajazleung <joshuajazleung@gmail.com>
2016-12-25 17:00:11 +0800

    add form for adding messages and how incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,19 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (data) {
   console.log('Got new message', data)
+  var li = jQuery('<li></li>')
+  li.text(`${data.from}: ${data.text}`)
+
+  jQuery('#messages').append(li)
+})
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault()
+
+  socket.emit('createMessage', {
+    from: 'Josh',
+    text: $('[name=message]').val()
+  }, (data) => {
+    console.log('Acknowledgement', data)
+  })
 })
--- server/server.js
+++ server/server.js
@@ -24,9 +24,10 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
-  socket.on('createMessage', (data) => {
+  socket.on('createMessage', (data, callback) => {
     console.log('New email created', data)
     io.emit('newMessage', generateMessage(data.from, data.text))
+    callback('I have acknowledged this.')
   })
 })
 

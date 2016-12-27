4e43fba25d39fe95238741bdacd234ef4ce6948b
joshuajazleung <joshuajazleung@gmail.com>
2016-12-27 19:48:48 +0800

    add autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 var socket = io()
 
+function scrollToBottom() {
+  // selectors
+  var messages = jQuery('#messages')
+  var newMessage = messages.children('li:last-child')
+
+  // height
+  var clientHeight = messages.prop('clientHeight')
+  var scrollTop = messages.prop('scrollTop')
+  var scrollHeight = messages.prop('scrollHeight')
+  var newMessageHeight = newMessage.innerHeight()
+  var lastMessageHeight = newMessage.prev().innerHeight()
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight)
+  }
+
+}
+
 socket.on('connect', function () {
   console.log('connected to the server')
 })
@@ -18,6 +36,7 @@ socket.on('newMessage', function (data) {
   })
 
   jQuery('#messages').append(html)
+  scrollToBottom()
 })
 
 socket.on('newLocationMessage', function (data) {
@@ -30,6 +49,7 @@ socket.on('newLocationMessage', function (data) {
   })
 
   jQuery('#messages').append(html)
+  scrollToBottom()
 })
 
 jQuery('#message-form').on('submit', function (e) {
